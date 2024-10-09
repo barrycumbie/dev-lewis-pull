b1d944d455f101261c0ca2e0a79653c39dbbebfa
Jacob Parry <54328477+Jacob-Vance@users.noreply.github.com>
2024-10-09 09:48:36 -0500

    Update script.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -12,33 +12,11 @@ $(document).ready(function(){
         + $('#email').val() + ', or your phone number, ' + $('#phone').val() + ', as soon as possible.'
      );
     };
-    
-// click the update... /upate send the form... l
-    document.getElementById("fnameBtn").onclick = function() {
-      sendData()
-    };
-
-    function sendData() {
-      
-    }
 
     // Load Data Button
     document.getElementById("loadBtn").onclick = function() {
       loadData()
     };
-  
-    // function loadData() {
-    //   document.getElementById('fname').value = 'Jacob'
-    //   document.getElementById('lname').value = 'Parry'
-    //   document.getElementById('email').value = 'jparry@una.edu'
-    //   document.getElementById('street').value = '3025 Sandy Flat Rd'
-    //   document.getElementById('city').value = 'Selmer'
-    //   document.getElementById('state').value = 'Tennessee'
-    //   document.getElementById('zip').value = '38375'
-    //   document.getElementById('physican').value = 'Dr. Jamie'
-    //   document.getElementById('insurance').value = 'Aetna'
-    //   document.getElementById('phone').value = '453-5580'
-    // };
   
     // Dependent input 
     $("input[type=radio][name=relationship]").on("change", function () {
